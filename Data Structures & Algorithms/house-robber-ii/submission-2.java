3657919ee8c41fbbe33963255bd3de41e3165ed5
kShaman771 <shamankannan@gmail.com>
2026-07-27 20:46:29 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        return Math.max(ansLin(nums,0,n-2),ansLin(nums,1,n-1));
+        
+    }
+
+    private int ansLin(int[] nums,int start,int end){
+        int prev1=0;
+        int prev2=0;
+        for(int i=start;i<=end;i++){
+            int cur=Math.max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=cur;
+        }
+
+        return prev1;
+    }
+}
